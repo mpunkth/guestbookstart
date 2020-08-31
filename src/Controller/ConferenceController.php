b84aab5458b00c0495acda6ebd07a9d206021f0a
root <root@vmi310155.contaboserver.net>
2020-08-31 11:31:45 +0200

    edit ConferenceController

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
