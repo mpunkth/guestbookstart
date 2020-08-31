16674395f852fec3446851a8c840021d9feef2ff
root <root@vmi310155.contaboserver.net>
2020-08-31 11:31:13 +0200

    edit ConferenceController

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,17 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
++use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
     /**
-     * @Route("/conference", name="conference")
+     * @Route("/", name="homepage")
      */
     public function index()
     {
-        return $this->render('conference/index.html.twig', [
-            'controller_name' => 'ConferenceController',
-        ]);
+        return new Response(<<<EOF      
+            <html>
+                <body>
+                    <img src="/images/under-construction.gif" />
+                </body>
+            </html>
+            EOF
+        );
     }
 }
